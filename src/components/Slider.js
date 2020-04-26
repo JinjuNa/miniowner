88d2048b83331ecbbbfde9ad67553375ea56fbbf
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-04-26 17:29:56 +0900

    slide 컴포넌트 분리, 진행중

---
+++ src/components/Slider.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+
+const Slide = styled.div`
+  width: 100%;
+  height: 70vh;
+`;
+
+const Container = styled.div`
+  width: 60%;
+  overflow: hidden; // 선을 넘어간 이미지들은 보이지 않도록 처리합니다.
+`;
+
+const Button = styled.button`
+  all: unset;
+  border: 1px solid coral;
+  padding: 0.5em 2em;
+  color: coral;
+  border-radius: 10px;
+  &:hover {
+    transition: all 0.3s ease-in-out;
+    background-color: coral;
+    color: #fff;
+  }
+`;
+
+const SliderContainer = styled.div`
+  width: 100%;
+  display: flex; //이미지들을 가로로 나열합니다.
+`;
+
+const CarouselComponent = (props) =>{
+  return (
+    <Slide>
+        <h3>{props.label}</h3>
+        <p>{props.detail}</p>
+    </Slide>
+  )
+}
+
+const TOTAL_SLIDES = 2;
+
+const Slider = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const slideRef = useRef(null);
+
+    const nextSlide = () => {
+        if (currentSlide >= TOTAL_SLIDES) { // 더 이상 넘어갈 슬라이드가 없으면 슬라이드를 초기화합니다.
+            setCurrentSlide(0);
+        } else {
+            setCurrentSlide(currentSlide + 1);
+        }
+    };
+
+    const prevSlide = () => {
+        if (currentSlide === 0) {
+            setCurrentSlide(TOTAL_SLIDES);
+        } else {
+            setCurrentSlide(currentSlide - 1);
+        }
+    };
+    
+
+    const slideItem = [
+      {num : 1, label : 'First slide label', detail : '첫번째 슬라이더에 관련된 내용입니다.', color: 'blue'},
+      {num : 2, label : 'Second slide label', detail : '두번째 슬라이더에 관련된 내용입니다.', color : 'red'},
+      {num : 3, label : 'Third slide label', detail : '세번째 슬라이더에 관련된 내용입니다.', color : 'yellow'}
+    ]
+    const slideContent = slideItem.map((slideItem, index)=> <CarouselComponent label={slideItem.label} detail={slideItem.detail} key={index} />);
+    
+    useEffect(() => {
+        slideRef.current.style.transition = "all 0.5s ease-in-out";
+        slideRef.current.style.transform = `translateX(-${currentSlide}00%)`; // 백틱을 사용하여 슬라이드로 이동하는 애니메이션을 만듭니다.
+      }, [currentSlide]);
+    
+    return(
+      <Container>
+        {currentSlide}
+        <SliderContainer ref={slideRef}>
+            {slideContent}
+        </SliderContainer>
+        <Button onClick={prevSlide}>Previous Slide</Button>
+        <Button onClick={nextSlide}>Next Slide</Button>
+        
+      </Container>
+    )
+}
+
+export default Slider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,44 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import ProjectComponent from '../components/ProjectComponent';
 import './Home.css';
+import Slider from '../components/Slider';
 
-const CarouselImg = styled.div`
-  width:800px;
-  height:400px;
-  background-color:#666;
-`
-
-const CarouselComponent = (props) =>{
-  return (
-    <div>
-      <CarouselImg className="d-block w-100"/>
-      <div>
-        <h3>{props.label}</h3>
-        <p>{props.detail}</p>
-      </div>
-    </div>
-  )
-}
-
-const CarouselBox = () => {
-    const slideItem = [
-      {num : 1, label : 'First slide label', detail : '첫번째 슬라이더에 관련된 내용입니다.', color: 'blue'},
-      {num : 2, label : 'Second slide label', detail : '두번째 슬라이더에 관련된 내용입니다.', color : 'red'},
-      {num : 3, label : 'Third slide label', detail : '세번째 슬라이더에 관련된 내용입니다.', color : 'yellow'}
-    ]
-    const slideContent = slideItem.map((slideItem, index)=> <CarouselComponent label={slideItem.label} detail={slideItem.detail} key={index} />);
-    return(
-      <div>
-        {slideContent}
-      </div>
-    )
-}
 
 const Home = () => {
   return (
     <div className="pageContainer">
-        <CarouselBox />
-
+        <Slider />
         <section className="projectSection">
           <h2>미니인턴을 신청하세요</h2>
           <p>이제 집, 학교, 도서관, 카페에서 인턴십을 경험하고, 실무역량평가로 취업하세요.</p>
