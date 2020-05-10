2fdbbc5affb1093ee4fd81aa027df3ae0d29799e
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-05-10 21:22:22 +0900

    slide component  , pager 작업

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,42 +1,79 @@
 import React, { useState, useEffect, useRef } from "react";
 import './Slider.css';
+import banner1 from '../banner1.jpg';
+import banner2 from '../banner2.jpg';
+import banner3 from '../banner3.jpg';
+import styled from "styled-components";
+
+const Slide = styled.div`
+  background-image: url("${props => props.img}");
+  background-size:cover;
+`
+
+const PagerContainer = styled.div`
+  position: absolute;
+  bottom:20px;
+  left: 0;
+  text-align : center;
+  width:100%;
+
+  & span{
+    display:inline-block;
+    width:15px;
+    height:15px;
+    background-color : #999;
+    opacity:0.5;
+    border-radius:50%;
+    font-size:0;
+    text-indent:-9999px;
+    margin: 10px;
+  }
+
+  & span.active {
+    background-color : #fff;
+    opacity : 1;
+  }
+`
 
 const CarouselComponent = ({slideItem}) =>{
   return (
-    <div className="slide" style={{backgroundColor : slideItem.color}}>
+    <Slide className="slide" img={slideItem.img} >
         <h3>{slideItem.label}</h3>
         <p>{slideItem.detail}</p>
-    </div>
+    </Slide>
   )
 }
+
 
 const TOTAL_SLIDES = 2;
 
 const Slider = () => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const slideRef = useRef(null);
-
-    const nextSlide = () => {
-        if (currentSlide >= TOTAL_SLIDES) { // 더 이상 넘어갈 슬라이드가 없으면 슬라이드를 초기화합니다.
-            setCurrentSlide(0);
-        } else {
-            setCurrentSlide(currentSlide + 1);
-        }
-    };
-
-    const prevSlide = () => {
-        if (currentSlide === 0) {
-            setCurrentSlide(TOTAL_SLIDES);
-        } else {
-            setCurrentSlide(currentSlide - 1);
-        }
-    };
+    const [activePage, setActivePage] = useState(1);
     
+    const pagerMenu = [
+      {id: 1, text : "1"},
+      {id: 2, text : "2"},
+      {id: 3, text : "3"}
+    ];
+    
+    const Pager = (() => {
+      return(
+        <PagerContainer>
+          {
+            pagerMenu.map((item, index) =>{
+                return <span key={index} className={activePage === item.id ? 'active' : ''} onClick={() => {setActivePage(item.id); setCurrentSlide(index)}}>{item.text}</span>
+            })
+          }
+        </PagerContainer>
+      )
+    }) 
 
     const slideItem = [
-      {num : 1, label : 'First slide label', detail : '첫번째 슬라이더에 관련된 내용입니다.', color: 'blue'},
-      {num : 2, label : 'Second slide label', detail : '두번째 슬라이더에 관련된 내용입니다.', color : 'red'},
-      {num : 3, label : 'Third slide label', detail : '세번째 슬라이더에 관련된 내용입니다.', color : 'yellow'}
+      {num : 1, label : 'First slide label', detail : '첫번째 슬라이더에 관련된 내용입니다.', color: 'blue', img : banner1 },
+      {num : 2, label : 'Second slide label', detail : '두번째 슬라이더에 관련된 내용입니다.', color : 'red', img : banner2},
+      {num : 3, label : 'Third slide label', detail : '세번째 슬라이더에 관련된 내용입니다.', color : 'yellow', img : banner3}
     ]
     const slideContent = slideItem.map((slideItem, index)=> <CarouselComponent slideItem={slideItem} key={index} />);
     
@@ -62,13 +99,10 @@ const Slider = () => {
 
     return(
       <div className="slider-container-wrap">
-        {currentSlide}
         <div className="slider-container" ref={slideRef}>
             {slideContent}
         </div>
-        <button className="slider-button" onClick={prevSlide}>Previous Slide</button>
-        <button className="slider-button" onClick={nextSlide}>Next Slide</button>
-        
+        <Pager />
       </div>
     )
 }
